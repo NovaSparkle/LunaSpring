d75cd44b005b86bc74b330344d3b62670c898cb7
NovaSparkle <novasparkly@gmail.com>
2024-12-18 16:49:58 +0500

    Release-2.3.1

---
+++ src/main/java/org/example/novasparkle/Util/Utils.java
@@ -0,0 +1,12 @@
+package org.example.novasparkle.Util;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+    public static int getIndex(int row, int col) {
+        return (row - 1) * 9 + col - 1;
+    }
+    public static String color(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+}
