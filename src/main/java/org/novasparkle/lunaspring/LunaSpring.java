17584eb806e0efe707edc525167d43cd99fcfc48
NovaSparkle <novasparkly@gmail.com>
2025-01-04 16:47:54 +0500

    Release-1.8.8

--- src/main/java/org/novasparkle/lunaspring/LunaSpring.java
+++ src/main/java/org/novasparkle/lunaspring/LunaSpring.java
@@ -2,13 +2,12 @@ package org.novasparkle.lunaspring;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.novasparkle.lunaspring.Events.MenuHandler;
 
 public final class LunaSpring extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.registerEvent(new MenuHandler());
+
     }
 
     @Override
