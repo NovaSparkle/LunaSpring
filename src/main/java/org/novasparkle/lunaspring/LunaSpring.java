74552013129c316f31c5206d5221aa242a53d68a
NovaSparkle <novasparkly@gmail.com>
2025-01-30 14:14:01 +0500

    Release-2.1.10

--- src/main/java/org/novasparkle/lunaspring/LunaSpring.java
+++ src/main/java/org/novasparkle/lunaspring/LunaSpring.java
@@ -1,7 +1,6 @@
 package org.novasparkle.lunaspring;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.novasparkle.lunaspring.Events.MenuHandler;
@@ -16,14 +15,14 @@ public final class LunaSpring extends JavaPlugin {
     @Getter
     private static final ServiceProvider serviceProvider = new ServiceProvider();
     @Getter
-    @Setter
     private static Plugin plugin = null;
 
     public void onEnable() {
         INSTANCE = this;
     }
 
-    public static MenuHandler initialize() {
+    public static MenuHandler initialize(Plugin plugin) {
+        LunaSpring.plugin = plugin;
         List<String> startMessage = INSTANCE.getConfig().getStringList("startMessage");
         startMessage.replaceAll(m -> m.replace("[pluginName]", plugin.getName()).replace("[pluginVersion]", plugin.getDescription().getVersion()).replace("[LSVersion]", INSTANCE.getDescription().getVersion()));
         startMessage.forEach(Utils::info);
