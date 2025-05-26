b439d8fda69000b2ed409ae3a5f1ef52d7077cdd
NovaSparkle <novasparkly@gmail.com>
2025-05-26 22:06:20 +0500

    3.4.3

--- src/main/java/org/novasparkle/lunaspring/API/util/utilities/Localization.java
+++ src/main/java/org/novasparkle/lunaspring/API/util/utilities/Localization.java
@@ -1,10 +1,15 @@
 package org.novasparkle.lunaspring.API.util.utilities;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.novasparkle.lunaspring.API.configuration.Configuration;
 import org.novasparkle.lunaspring.LunaSpring;
 
 public class Localization {
-    public static <R extends Enum<R>> String localize(R type) {
-        return new Configuration(LunaSpring.getINSTANCE().getDataFolder(), "localization").getString(String.format("%s.%s", type.getClass().getSimpleName(), type.name()));
+    public static String localize(EntityType type) {
+        return new Configuration(LunaSpring.getINSTANCE().getDataFolder(), "localization").getString(String.format("EntityType.%s", type.name()));
+    }
+    public static String localize(Enchantment enchantment) {
+        return new Configuration(LunaSpring.getINSTANCE().getDataFolder(), "localization").getString(String.format("Enchantment.%s", enchantment.getKey().getKey()));
     }
 }
