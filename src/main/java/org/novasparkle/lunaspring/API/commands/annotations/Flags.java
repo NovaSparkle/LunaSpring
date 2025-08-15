28af138ea54ece57efc9dd43672ba0a4b9de5372
NovaSparkle <novasparkly@gmail.com>
2025-08-16 01:20:40 +0500

    3.5.19f

--- src/main/java/org/novasparkle/lunaspring/API/commands/annotations/Flags.java
+++ src/main/java/org/novasparkle/lunaspring/API/commands/annotations/Flags.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Flags {
-    ZeroArgCommand.AccessFlag[] value();
+    ZeroArgCommand.AccessFlag[] value() default {};
 }
--- src/main/java/org/novasparkle/lunaspring/API/commands/annotations/TabCompleteIgnore.java
+++ src/main/java/org/novasparkle/lunaspring/API/commands/annotations/TabCompleteIgnore.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface TabCompleteIgnore {
-    String[] value();
+    String[] value() default {};
 
 }
