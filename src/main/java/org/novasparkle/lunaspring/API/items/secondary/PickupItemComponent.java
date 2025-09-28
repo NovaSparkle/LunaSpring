09a8e24574e6d93efb909d4c6887c58a0f351a46
ProGiple <ghosthostgtop@gmail.com>
2025-09-28 23:08:55 +0300

    3.8.1

--- src/main/java/org/novasparkle/lunaspring/API/items/secondary/PickupItemComponent.java
+++ src/main/java/org/novasparkle/lunaspring/API/items/secondary/PickupItemComponent.java
@@ -3,9 +3,9 @@ package org.novasparkle.lunaspring.API.items.secondary;
 import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.novasparkle.lunaspring.API.items.Component;
-import org.novasparkle.lunaspring.API.items.SlotFilteringComponent;
+import org.novasparkle.lunaspring.API.items.ItemComponent;
 
 @Component
-public interface PickupItemComponent extends SlotFilteringComponent {
+public interface PickupItemComponent extends ItemComponent {
     void onPickup(PlayerAttemptPickupItemEvent e, ItemStack itemStack);
 }
